321c6bb2269a71c005fa51c9cfbac411fec408b1
mario-alexx <marioatoroh@gmail.com>
2024-05-31 00:11:18 -0300

    adding user service

--- WebStore.Application/Interfaces/IUserService.cs
+++ WebStore.Application/Interfaces/IUserService.cs
@@ -5,14 +5,21 @@ public interface IUserService{
     /// <summary>
     /// Registers a new user.
     /// </summary>
-    /// <param name="userDto">User data transfer object</param>
+    /// <param name="registerModel">User data transfer object</param>
     /// <returns>Task representing the asynchronous operation</returns>
-    Task RegisterAsync(UserRequestModel userDto);
+    Task<OperationResult<RegisterModel>> RegisterAsync(RegisterModel registerModel);
 
     /// <summary>
     /// Logs in a user.
     /// </summary>
-    /// <param name="loginDto">Login data transfer object</param>
+    /// <param name="loginModel">Login data transfer object</param>
     /// <returns>Task representing the asynchronous operation, containing a JWT token if successful</returns>
-    Task<string> LoginAsync(LoginModel loginDto);
+    Task<OperationResult<string>> LoginAsync(LoginModel loginModel);
+
+    /// <summary>
+    /// Updates user profile details.
+    /// </summary>
+    /// <param name="userRequestModel">User data transfer object.</param>
+    /// <returns>Task representing the asynchronous operation.</returns>
+    Task<OperationResult<UserRequestModel>> UpdateProfileAsync(UserRequestModel userRequestModel);
 }
