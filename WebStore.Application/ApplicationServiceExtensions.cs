c05b505d976c41626c6f584da9d766243f624f84
mario-alexx <marioatoroh@gmail.com>
2024-05-31 00:13:23 -0300

    adding methods extensions in the application

---
+++ WebStore.Application/ApplicationServiceExtensions.cs
@@ -0,0 +1,33 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
+
+public static class ApplicationServiceExtension {
+
+    /// <summary>
+    /// Adds application services to the specified IServiceCollection.
+    /// </summary>
+    /// <param name="services">The IServiceCollection to add services to.</param>
+    public static void AddApplicationService(this IServiceCollection services) {
+
+        services.AddScoped<IUserService, UserService>();
+        services.AddScoped<ITokenService, TokenService>();
+
+        //AutoMapper register
+        //services.AddAutoMapper(typeof(UserProfile));
+        services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+        // Register FluentValidation
+        services.AddValidatorsFromAssemblyContaining<UserModelValidator>();
+        services.AddValidatorsFromAssemblyContaining<LoginModelValidator>();
+
+        //FluentValidation
+        services.AddTransient<IValidator<UserRequestModel>, UserModelValidator>();
+        services.AddTransient<IValidator<LoginModel>, LoginModelValidator>();
+        services.AddTransient<IValidator<RegisterModel>, RegisterModelValidator>();
+
+        // FluentValidation MVC integration
+        services.AddFluentValidationAutoValidation();
+        //services.AddFluentValidationClientsideAdapters();
+    }
+}
